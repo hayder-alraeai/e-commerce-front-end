22fd26b7c78b3846027f6dc98f1a524cfd9f1159
hayder-alraeai <haydar860116@hotmail.com>
2021-05-21 13:38:38 +0200

    changed backend to heroku

--- src/config/Config.js
+++ src/config/Config.js
@@ -1,8 +1,8 @@
 //works at online server
-// export const backendPath = "https://e-commerce-de.herokuapp.com"
+export const backendPath = "https://e-commerce-de.herokuapp.com"
 
 //works localhost
 // export const backendPath = "http://localhost:8080"
 
 //works at aws
-export const backendPath = "https://e-commerce-backend.alraeai.se"
+// export const backendPath = "https://e-commerce-backend.alraeai.se"
